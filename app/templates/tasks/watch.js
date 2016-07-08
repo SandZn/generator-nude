9c29f5e3ad0909454f107072df1af3f399b70e1a
Darlan Mendonça <darlanmendonca@gmail.com>
2016-07-08 12:12:53 -0300

    update npm versions in travis CI

--- app/templates/tasks/watch.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let gulp = require('gulp');
-let gulpConfig = require('./gulp.config.js');
-
-gulp.task('watch', function() {
-	gulp.watch(gulpConfig.lint, ['lint']);
-	gulp.watch('./server/**/*.controller.js', ['apiDocs']);
-});
--- tasks/watch.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let gulp = require('gulp');
-let config = require('./config.js');
-
-gulp.task('watch', function() {
-  gulp.watch(config.lint, ['lint']);
-});
