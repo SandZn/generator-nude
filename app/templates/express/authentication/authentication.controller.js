9642e0c3bc0dcdb7f2d76228f93e70eb768c4b0a
Darlan Mendonça <darlanmendonca@gmail.com>
2016-08-14 22:47:57 -0300

    simple refactor to callbacks in promise, from authentication controller

--- app/templates/express/authentication/authentication.controller.js
+++ app/templates/express/authentication/authentication.controller.js
@@ -4,7 +4,6 @@ let Users = require('../users/users.model.js');
 let config = require('../../config');
 let bluebird = require('bluebird');
 let jwt = require('jsonwebtoken');
-jwt = bluebird.promisifyAll(jwt);
 let encode = require('../encode/encode.helper.js');
 let publicFields = '-__v -password';
 
@@ -23,26 +22,25 @@ function local(req, res) {
     * @apiParam {String} email email of user
     * @apiParam {String} password password of user
     */
-  let password = encode(req.body.password);
+
   let email = req.body.email;
+  let password = encode(req.body.password);
 
   Users
     .findOne({email, password}, publicFields)
-    .then(function(user) {
-      if (!user) {
-        let message = 'authentication failed';
-        return res
-          .status(401)
-          .json({message});
-      }
+    .then(response);
 
-      let id = user.id;
-      let token = jwt.sign(user, config.secret, config.token);
+  function response(user) {
+    if (!user) {
+      let message = 'authentication failed';
+      return res
+        .status(401)
+        .json({message});
+    }
 
-      res.json({
-        id,
-        token,
-      });
+    let id = user.id;
+    let token = jwt.sign(user, config.secret, config.token);
 
-    });
+    res.json({id, token});
+  }
 };
