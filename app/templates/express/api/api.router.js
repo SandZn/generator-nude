56ba82706955dba27c99531eacc82139a61b9d31
Darlan Mendonça <darlanmendonca@gmail.com>
2016-11-11 10:56:33 -0200

    add default argument to encode

--- app/templates/express/api/api.router.js
+++ app/templates/express/api/api.router.js
@@ -1,27 +1,27 @@
-import api from '../controllers.js';
-import middlewares from '../middlewares.js';
+import {users} from '../controllers.js';
+import {id, token} from '../middlewares.js';
 import {Router} from 'express';
 
 const router = Router();
 
-router.param('id', middlewares.id);
+router.param('id', id);
 
 router
   .route('/users/authentication')
-  .post(api.users.authenticate);
+  .post(users.authenticate);
 
-router.use(middlewares.token);
+router.use(token);
 
 router
   .route('/users')
-  .get(api.users.list)
-  .post(api.users.create);
+  .get(users.list)
+  .post(users.create);
 
 router
   .route('/users/:id')
-  .get(api.users.single)
-  .put(api.users.update)
-  .delete(api.users.remove);
+  .get(users.single)
+  .put(users.update)
+  .delete(users.remove);
 
 router.use(resourceNotFound);
 
--- app/templates/express/encode/encode.helper.js
+++ app/templates/express/encode/encode.helper.js
@@ -2,7 +2,7 @@ import crypto from 'crypto';
 
 module.exports = EncodeHelper;
 
-function EncodeHelper (str) {
+function EncodeHelper (str='') {
   return crypto
     .createHash('md5')
     .update(str)
