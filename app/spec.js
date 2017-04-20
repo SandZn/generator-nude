1b421e71b502d728bf1b19acfc19c35ac0e5df8c
Darlan Mendonça <darlanmendonca@gmail.com>
2017-04-20 11:21:49 -0300

    remove gulp and begin changes to remove babel

---
+++ app/spec.js
@@ -0,0 +1,44 @@
+const test = require('yeoman-test')
+const assert = require('yeoman-assert')
+const packageJSON = require('../package.json')
+
+const generator = `${process.env.PWD}/${packageJSON.main}`
+
+describe('generate a scaffolding', scaffolding)
+
+function scaffolding() {
+  const params = {
+    applicationName: 'lorem',
+    appSecret: 'lorem',
+  }
+
+  it('files', done => {
+    test
+      .run(generator)
+      .withPrompts(params)
+      .toPromise()
+      .then(assertionFiles)
+
+    function assertionFiles() {
+      assert.file([
+        '.editorconfig',
+        '.gitignore',
+        '.eslintrc.js',
+        '.yo-rc.json',
+        'README.md',
+        'app',
+        'config.js',
+        'app/controllers.js',
+        'docs',
+        'gulpfile.babel.js',
+        'app/middlewares.js',
+        'package.json',
+        'app/routers.js',
+        'test',
+        'tasks',
+      ])
+
+      done()
+    }
+  })
+}
--- tasks/watch.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp'
-import {lint as scripts} from './config.js'
-
-gulp.task('watch', watchTask)
-
-function watchTask() {
-  return gulp.watch(scripts, ['lint'])
-}
